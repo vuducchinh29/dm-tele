bfc9a9212d5684bf8241d98c96460619bd2b79e7
vuducchinh29 <vuducchinh29@gmail.com>
2020-07-29 01:00:30 +0700

    get all contacts of user

--- src/components/searchChatComponents/SearchChatList.js
+++ src/components/searchChatComponents/SearchChatList.js
@@ -3,28 +3,47 @@ import { ScrollView } from 'react-native';
 import SearchChatItem from './SearchChatItem';
 import SearchChatAddItem from './SearchChatAddItem';
 
+import { API } from '../../assets/constants'
+import Utils from '../../assets/utils'
 class SearchChatList extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+  componentDidMount = async () => {
+    const user_info_str = await Utils.getData('user_info')
+    const user_info = JSON.parse(user_info_str)
+    API.call('contacts.getContacts', {
+      hash: user_info.access_hash
+    })
+    .then((response) => {
+      console.log(response);
+      this.setState({
+        listUsers: response.users
+      })
+    })
+    .catch((err) => console.error(err));
+  };
+  
 
   render() {
+    const { listUsers } = this.state;
     return (
       <ScrollView>
         <SearchChatAddItem icon="user" title="New Contact" />
-        <SearchChatItem
-          imageSrc="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-          username="Bruce Mars"
-          bio="my name is someone, i work at Tatty DEV"
-          isBlocked
-          isMuted
-        />
-        <SearchChatItem
-          imageSrc="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-          username="Bayar Botany"
-          bio="Founder at Tatty DEV"
-        />
+        {
+          listUsers && listUsers.map((user, key) => (
+            <SearchChatItem
+              key={key}
+              imageSrc="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              username={user.first_name || user.last_name}
+              user_id={user.id}
+              bio="my name is someone, i work at Tatty DEV"
+              isBlocked
+              isMuted
+            />
+          ))
+        }
       </ScrollView>
     );
   }
