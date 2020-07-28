46e266d062a0a9f7a3f9409fd5e1b18e5172db83
vuducchinh29 <vuducchinh29@gmail.com>
2020-07-28 23:40:12 +0700

    use API; save user_info into storage

--- src/pages/VerificationPage.js
+++ src/pages/VerificationPage.js
@@ -1,40 +1,35 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import AuthHeader from '../components/loginPageComponents/AuthHeader';
 import TextInputComponent from '../components/loginPageComponents/TextInputComponent';
 import BigButtonComponent from '../components/loginPageComponents/BigButtonComponent';
 import { theme } from '../theme';
+import Utils from '../assets/utils'
+import { API } from '../assets/constants'
 
-import { MTProto } from '@mtproto/core'
-const api_id = '1419179';
-const api_hash = '0fbc49808dfa383bd1a3b381493ef836';
-const mtproto = new MTProto({
-  api_id,
-  api_hash,
-  test: true,
-});
 class VerificationPage extends Component {
-    // THIS OPTION IS TO SHOW/HIDE DEFAULT NAVIGATION HEADER
     static navigationOptions = {
         header: null
     };
     constructor(props) {
         super(props);
         this.state = {
-            isLoading: true
+            isLoading: false
         };
     }
 
     signIn = () => {
+        this.setState({isLoading: true})
         const { phone_number, phone_code_hash } = this.props.navigation.getParam('payload');
         const { phone_code } = this.state
-        mtproto.call('auth.signIn', {
+        API.call('auth.signIn', {
             phone_code: phone_code,
             phone_number: phone_number,
             phone_code_hash: phone_code_hash,
         })
         .then((res) =>{
-            console.log('Signed in successfully!');
+            console.log('Signed in successfully!', res);
+            Utils.saveData('user_info', res.user)
             this.setState({
                 isLoading: false,
             })
@@ -56,17 +51,23 @@ class VerificationPage extends Component {
         return (
             <View style={{ flex: 1, height: '100%' }} >
                 <View style={{ flex: 2 }}>
-                    {
-                        isLoading ?
-                        <AuthHeader icon='check' title="Loading..." /> :
-                        <AuthHeader icon='check' title="Verification" />
-                    }
+                    <AuthHeader icon='check' title="Verification" />
                     <TextInputComponent 
                         from='verify'
                         title='Code' 
                         placeholder='12345'
                         getPhoneCode={(value) => this.getPhoneCode(value)}
                     />
+                    {
+                        isLoading && <Text 
+                        style={{
+                        color: theme.colors.primary,
+                        fontWeight: 'bold',
+                        fontSize: 20,
+                        marginTop: 30,
+                        textAlign: 'center'
+                        }}>Verifing the code...</Text>
+                    }
                 </View>
                 <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
                     <BigButtonComponent
