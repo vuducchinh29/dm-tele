95dc1a82be0db31af2dfe7c7ce0551b3f26d9192
vuducchinh29 <vuducchinh29@gmail.com>
2020-07-29 00:59:18 +0700

    set default localStorage is AsyncStorage

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,8 +1,11 @@
 import { MTProto } from '@mtproto/core'
+import AsyncStorage from '@react-native-community/async-storage'
+
 const api_id = '1419179';
 const api_hash = '0fbc49808dfa383bd1a3b381493ef836';
 
 export const API = new MTProto({
   api_id,
   api_hash,
+  customLocalStorage: AsyncStorage
 });;
