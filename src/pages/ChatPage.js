9983b27c94f90f18ff8846d7199f598224d4fa3c
vuducchinh29 <vuducchinh29@gmail.com>
2020-08-04 01:05:24 +0700

    save chat_info and put into MessageView

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -6,9 +6,12 @@ import MessagesView from '../components/chatComponents/MessagesView';
 import { theme } from '../theme';
 
 import { API } from '../assets/constants'
-import Utils from '../assets/utils'
 
 class ChatPage extends Component {
+  state = {
+    chat_info: null
+  }
+
   static navigationOptions = {
     header: null
   };
@@ -28,6 +31,9 @@ class ChatPage extends Component {
     })
     .then ((res) => {
       console.log(res);
+      this.setState({
+        chat_info: res
+      })
     })
     .catch((err) => console.error(err))
   };
@@ -37,16 +43,16 @@ class ChatPage extends Component {
     const { navigation } = this.props;
     const username = navigation.getParam('username', 'no name available');
     const user_id = navigation.getParam('user_id', 'no id available');
+    const access_hash = navigation.getParam('access_hash', 'no id available');
     const status = navigation.getParam('status', 'no id available');
     const bio = navigation.getParam('bio', 'No Bio Available');
     const imageSrc = navigation.getParam('imageSrc', null);
     const isBlocked = navigation.getParam('isBlocked', false);
     const isMuted = navigation.getParam('isMuted', false);
+    const {chat_info} = this.state
     return (
       <View style={{ flex: 1 }}>
-        {/* The Color of Status Bar (battery level, notifcitaion bar, and wifi stats) */}
         <StatusBar barStyle="dark-content" backgroundColor={theme.colors.tabPageBackground} />
-        {/* CHAT_HEADER Component (PICTURE, ONLINE_STATUS, CALL, ATTACHMENTS, OPTIONS BUTTON) */}
         <ChatHeader
           onOpenProfile={() =>
             this.props.navigation.navigate('FriendsProfilePage', {
@@ -64,9 +70,13 @@ class ChatPage extends Component {
           imageSrc={imageSrc}
           onlineStatus={status}
         />
-        {/* Messages_AREA component where u get all the messages */}
-        <MessagesView />
-        {/* CHAT_INPUT component which includes Typing, Microphone and send Button */}
+        {chat_info && 
+        <MessagesView 
+          chat_info={chat_info}
+          username={username}
+          user_id={user_id}
+          access_hash={access_hash}
+        />}
         <ChatInput />
       </View>
     );
