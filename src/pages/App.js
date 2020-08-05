504d9dc99060802703726fd9b1c273361b984447
vuducchinh29 <vuducchinh29@gmail.com>
2020-08-06 05:48:40 +0700

    getState to set the default page

--- src/pages/App.js
+++ src/pages/App.js
@@ -20,64 +20,102 @@ import StoryView from './StoryView';
 import LoginPage from './LoginPage';
 import VerificationPage from './VerificationPage';
 import SignUpPage from './SignUpPage';
+import InitPage from './InitPage';
+
+import { API } from '../assets/constants';
 
 // NAVIGATOR FUNCTION
-const ChatPageNavigator = createStackNavigator(
-  {
-    Home: createMaterialTopTabNavigator(
-      {
-        Calls: CallsTab,
-        Chats: ConversationsTab,
-        Stories: StoriesTab
-      },
-      {
-        initialRouteName: 'Chats',
-        tabBarOptions: {
-          activeTintColor: theme.colors.activeTabTitle,
-          inactiveTintColor: theme.colors.inactiveTabTitle,
-          tabStyle: {
-            backgroundColor: 'transparent'
-          },
-          labelStyle: {
-            fontWeight: 'bold'
-          },
-          indicatorStyle: {
-            backgroundColor: theme.colors.tabIndicator
-          },
-          style: {
-            backgroundColor: theme.colors.tabBackground
-          }
-        },
-        navigationOptions: {
-          /* HEADER Component
-        The onPress function is to
-        navigate to ProfilePage when you click on the profile picture */
-          header: <Header title="BlueChat" />
-        }
-      }
-    ),
-    MessagePage: ChatPage,
-    ProfilePage,
-    FriendsProfilePage,
-    SettingsPage,
-    OnCallPage,
-    SearchChatPage,
-    StoryView,
-    LoginPage,
-    VerificationPage,
-    SignUpPage
-    
-  },
-  {
-    //THE DEFAULT PAGE WHEN APP STARTS
-    initialRouteName: 'Home'
-  }
-);
-
-const AppContainer = createAppContainer(ChatPageNavigator);
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appState: 'init'
+    }
+  }
+  
+  componentDidMount = () => {
+    API.call('updates.getState', {})
+    .then((state) => {
+      console.log('state', state)
+      this.setState({
+        appState: 'home'
+      })
+    })
+    .catch((err) => {
+      console.log('error', err)
+      if (err.error_message === "AUTH_KEY_UNREGISTERED") {
+        this.setState({
+          appState: 'login'
+        })
+      }
+    })
+  };
+  
+  
   render() {
+    const { appState } = this.state
+    let defaultState
+    switch (appState) {
+      case 'login':
+        defaultState = 'LoginPage'
+        break;
+      case 'home':
+        defaultState = 'Home'
+        break;
+      default:
+        defaultState = 'InitPage'
+        break;
+    }
+    const ChatPageNavigator = createStackNavigator(
+      {
+        Home: createMaterialTopTabNavigator(
+          {
+            Calls: CallsTab,
+            Chats: ConversationsTab,
+            Stories: StoriesTab
+          },
+          {
+            initialRouteName: 'Chats',
+            tabBarOptions: {
+              activeTintColor: theme.colors.activeTabTitle,
+              inactiveTintColor: theme.colors.inactiveTabTitle,
+              tabStyle: {
+                backgroundColor: 'transparent'
+              },
+              labelStyle: {
+                fontWeight: 'bold'
+              },
+              indicatorStyle: {
+                backgroundColor: theme.colors.tabIndicator
+              },
+              style: {
+                backgroundColor: theme.colors.tabBackground
+              }
+            },
+            navigationOptions: {
+              header: <Header title="Private Chat" />
+            }
+          }
+        ),
+        MessagePage: ChatPage,
+        ProfilePage,
+        FriendsProfilePage,
+        SettingsPage,
+        OnCallPage,
+        SearchChatPage,
+        StoryView,
+        LoginPage,
+        VerificationPage,
+        SignUpPage,
+        InitPage
+      },
+      {
+        //THE DEFAULT PAGE WHEN APP STARTS
+        initialRouteName: defaultState
+      }
+    );
+    const AppContainer = createAppContainer(ChatPageNavigator);
     return (
       <SafeAreaView style={{ backgroundColor: 'white', flex: 1 }}>
         <AppContainer />
